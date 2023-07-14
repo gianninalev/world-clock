c0971104b0942e587ff96fc58ed0c01371ddbb79
gianninalev <gialevano@gmail.com>
2023-07-14 10:15:47 +0200

    Added current location

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ setInterval(updateOsloTime, 1000);
 
 function updateSelectedCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
